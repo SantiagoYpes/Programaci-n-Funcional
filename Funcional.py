d1fec2db7b6912f0e50be78082992e6041a9cec9
SantiagoYpes <santiyezu8@hotmail.com>
2022-08-17 18:51:02 -0500

    Cambios

--- Funcional.py
+++ Funcional.py
@@ -9,6 +9,24 @@ def fibonacci(posicion):
 resultado = fibonacci(6)
 print("El resultado de la sucesión de fibonacci en la posición 6 es: ",resultado)
 
+def fibonacci_iter(n):
+    a=1
+    b=1
+    if n==1: 
+        print('0')
+    elif n==2:
+        print('0','1')
+    else:
+        print('0')
+        print(a)
+        print(b)
+        for i in range(n-3):
+            total = a + b
+            b=a
+            a= total
+            print(total)
+
+
 #Funciones Puras
 precio=120000
 
